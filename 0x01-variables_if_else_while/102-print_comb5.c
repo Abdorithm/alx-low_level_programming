e395e2ed5c070ce656a4ae1988d66c9963a730fd
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-03-18 04:02:12 +0200

    added num comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,28 @@
  */
 int main(void)
 {
-	int i = 0, j = 0;
+	int i, j, x, y, z;
 
-	for (i = 0; i <= 9; i++)
-		for (j = 0; j <= 9; j++)
+	for (i = 0; i <= 99; i++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-			if (!(i == 9 && j == 9))
+			x = i % 10;
+			z = i / 10;
+			y = z % 10;
+			putchar('0' + y);
+			putchar('0' + x);
+			putchar(' ');
+			x = j % 10;
+			z = j / 10;
+			y = z % 10;
+			putchar('0' + y);
+			putchar('0' + x);
+			if (!(i == 98 && j == 99))
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+	putchar('\n');
 	return (0);
 }
