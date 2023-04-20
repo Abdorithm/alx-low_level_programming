14dc0763e243bfd11e434ab8a4256796342c02bc
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-04-20 23:57:45 +0200

    string var function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - ...
+ * @separator: ...
+ * @n: ...
+ *
+ * Return: ...
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(ap, char *) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(ap, char *));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
