b5c8a54d7e7c47a11ab9259ce091e3b4954e443b
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-04-28 16:13:53 +0200

    linked list commits

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_list - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
+ * print_list - ...
+ * @h: ...
+ * Return: ...
  */
 size_t print_list(const list_t *h)
 {
