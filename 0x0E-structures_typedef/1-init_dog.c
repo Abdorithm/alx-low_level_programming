01ad3558df2869127e55e19e0ef45079185515f5
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-06-20 00:06:22 +0200

    typedef & structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = name; /* or (*d).name = name */
+		d->name = name; /* (*d).name = name works too */
 		d->age = age;
 		d->owner = owner;
 	}
