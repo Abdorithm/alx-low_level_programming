87ea4e64a5ed595338ba8416cd39f8df30ef1db4
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-07-07 01:47:17 +0200

    new file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,10 +7,9 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor, shift;
-	unsigned int j, i;
+	unsigned long int xor, shift, i;
+	unsigned int j = 0;
 
-	j = 0;
 	xor = n ^ m;
 	for (i = 0; i < 32; i++)
 	{
