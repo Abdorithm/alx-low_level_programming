9833084e790eeea03eec133e7c29a92c3fa07452
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-09-09 00:28:47 +0200

    print

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i;
+	int begin = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
+		{
+			if (begin)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			begin = 1;
+		}
+	printf("}\n");
+}
